a2f30edd3f80bac8cf5155dca189e9a2633fb3bb
shpark30 <seohyeonpark30@gmail.com>
2022-03-30 07:21:21 +0000

    final inference

--- inference.py
+++ inference.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import json
 import pandas as pd
 from tqdm import tqdm
 from pathlib import Path
@@ -24,30 +25,34 @@ def main(args):
     doc_id, doc = data.index.tolist(), data[['text_obj', 'text_mthd', 'text_deal']].fillna('')
     doc = doc.apply(lambda x: ' '.join(x), axis=1).tolist()
     
-    state = torch.load(os.path.join(args.model_path, 'weights/best.pth.tar'))
-    with open(os.path.join(args.model_path, 'id2cat.json', 'r')) as f:
+    checkpoint = torch.load(os.path.join(args.exp_path, 'weights/best.pth.tar'), map_location=args.device)
+    with open(os.path.join(args.exp_path, 'config.json'), 'r') as f:
+        checkpoint_args = json.load(f)
+    with open(os.path.join(args.exp_path, 'id2cat.json'), 'r') as f:
         id2cat = json.load(f)
     
-    if state['args'].model == 'kobert':
+    if checkpoint_args['model'] == 'kobert':
         kobert, vocab = get_pytorch_kobert_model()
         tokenizer_path = get_tokenizer()
         tokenizer = nlp.data.BERTSPTokenizer(tokenizer_path, vocab, lower=False)
         transform = nlp.data.BERTSentenceTransform(
-                    tokenizer, max_seq_length=state['args'].max_len, pad=True, pair=False) 
+                    tokenizer, max_seq_length=checkpoint_args['max_len'], pad=True, pair=False) 
         model = KOBERTClassifier(bert=kobert, num_classes=len(id2cat))
         model = model.to(args.device)
         dataset = KOBERTClassifyDataset(doc, doc_id, transform)
-    elif state['args'].model == 'kogpt2':
-        dataset = KOGPT2ClassifyDataset(doc, doc_id, max_len=state['args'].max_len, padding='max_length', truncation=True)
-        model = KOGPT2Classifier(num_cls=len(id2cat), pad_token_id = dataset.tokenizer.eos_token_id)
-    elif state['args'].model == 'kogpt3':
-        dataset = KOGPT3ClassifyDataset(doc, doc_id, max_len=state['args'].max_len, padding='max_length', truncation=True)
-        model = KOGPT3Classifier(num_cls=len(id2cat), pad_token_id = dataset.tokenizer.eos_token_id)
+    elif checkpoint_args['model'] == 'kogpt2':
+        dataset = KOGPT2ClassifyDataset(doc, doc_id, max_len=checkpoint_args['max_len'], padding='max_length', truncation=True)
+        model = KOGPT2Classifier(num_classes=len(id2cat), pad_token_id = dataset.tokenizer.eos_token_id)
+    elif checkpoint_args['model'] == 'kogpt3':
+        dataset = KOGPT3ClassifyDataset(doc, doc_id, max_len=checkpoint_args['max_len'], padding='max_length', truncation=True)
+        model = KOGPT3Classifier(num_classes=len(id2cat), pad_token_id = dataset.tokenizer.eos_token_id)
     else:
         raise
     dataloader = DataLoader(dataset, batch_size=args.batch_size, num_workers=args.workers, shuffle=False, pin_memory=False)
     
     model = model.to(args.device)
+    model.load_state_dict(checkpoint['state_dict'])
+    
     model.eval()
     with torch.no_grad():
         for input_ids, attention_mask, token_type_ids, doc_id in tqdm(dataloader, total=len(dataloader)):
@@ -58,7 +63,12 @@ def main(args):
             output = model(input_ids, attention_mask, token_type_ids)
             output = torch.argmax(output, 1)
             
-            output_cat = list(map(lambda x: id2cat[x], output.cpu().tolist()))
+            try:
+                output_cat = list(map(lambda x: id2cat[str(x)], output.cpu().tolist()))
+            except:
+                import pdb
+                pdb.set_trace()
+                output_cat = list(map(lambda x: id2cat[str(x)], output.cpu().tolist()))
             for r, digit in zip(doc_id.tolist(), output_cat):
                 data.loc[r, ['digit_1', 'digit_2', 'digit_3']] = digit
     
@@ -67,14 +77,14 @@ def main(args):
                 
 if __name__=='__main__':
     FILE = Path(__file__).resolve()
-    DATA = FILE.parents[1]
+    DATA = FILE.parents[2]
     ROOT = FILE.parents[0]  # root directory
     save_dir = increment_path(Path(ROOT) / 'runs' / 'inference' / 'exp')
     
     parser=argparse.ArgumentParser(
         description='')
 
-    parser.add_argument('--root', default= DATA / '2. 모델개발용자료.txt', type=str,
+    parser.add_argument('--root', default= DATA / 'data' / '2. 모델개발용자료.txt', type=str,
                         help='data format should be txt, sep="|"')
     parser.add_argument('--exp-path', required=True, type=str,
                        help='path of a directory which contains the "weights" folder and id2cat.json')
--- inference.sh
+++ inference.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
 
 BATCH=512
-EXP_PATH='./runs/models/exp4'
-eval 'python inference.py --exp-path=${EXP_PATH} --batch-size={BATCH}'
+EXP_PATH='./runs/train/exp4'
+eval 'python inference.py --exp-path=${EXP_PATH} --batch-size=${BATCH}'
 
-EXP_PATH='./runs/models/exp5'
-eval 'python inference.py --exp-path=${EXP_PATH} --batch-size={BATCH}'
-
-EXP_PATH='./runs/models/exp6'
-eval 'python inference.py --exp-path=${EXP_PATH} --batch-size={BATCH}'
+EXP_PATH='./runs/train/exp5'
+eval 'python inference.py --exp-path=${EXP_PATH} --batch-size=${BATCH}'
